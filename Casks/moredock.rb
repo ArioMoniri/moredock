d3a2f01eaaea0fb00f64e9ca968a4403b5d95457
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2026-07-08 16:26:47 +0300

    Update Homebrew cask checksum for 0.1.1

--- Casks/moredock.rb
+++ Casks/moredock.rb
@@ -1,6 +1,6 @@
 cask "moredock" do
   version "0.1.1"
-  sha256 "3d3d53c017a2e368b988e55a97c8e03e3bf72ba4b18d75b6bf73c1f6248d7497"
+  sha256 "2426d186733695ecf796dd906e40313fb358daa87c5499f69520eaed5f860531"
 
   url "https://github.com/ArioMoniri/moredock/releases/download/v#{version}/MoreDock-#{version}-macOS.dmg"
   name "MoreDock"
